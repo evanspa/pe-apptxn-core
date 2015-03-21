70b9ca11d84f0ec13e371f99ee8b84aaadd9452f
Paul Evans <evansp2@gmail.com>
2015-03-21 00:02:35 -0400

    updating deps and added release.sh

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/data.codec "0.1.0"]
-                 [pe-core-utils "0.0.5"]
+                 [pe-core-utils "0.0.7"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.slf4j/slf4j-api "1.7.5"]
                  [clj-time "0.8.0"]
@@ -18,7 +18,7 @@
                                org.slf4j/slf4j-log4j12]]]
   :resource-paths ["resources"]
   :codox {:exclude [user]
-          :src-dir-uri "https://github.com/evanspa/pe-apptxn-core/blob/0.0.1/"
+          :src-dir-uri "https://github.com/evanspa/pe-apptxn-core/blob/0.0.2/"
           :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:source-paths ["dev"]
                    :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
@@ -26,7 +26,7 @@
                                   [org.clojure/java.classpath "0.2.2"]
                                   [org.clojure/data.json "0.2.5"]
                                   [org.clojure/tools.nrepl "0.2.7"]
-                                  [pe-datomic-testutils "0.0.1"]]
+                                  [pe-datomic-testutils "0.0.2"]]
                    :resource-paths ["test-resources"]}
              :test {:resource-paths ["test-resources"]}}
   :repositories [["releases" {:url "https://clojars.org/repo"
